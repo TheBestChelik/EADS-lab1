d37c5e0c30dd54a4a7c733501384fedba72c7d2f
Oleksandr <vasa22292@gmail.com>
2023-11-08 10:26:20 +0100

    feat(split): add split pos and split key

--- split.hpp
+++ split.hpp
@@ -1,7 +1,10 @@
+#ifndef SPLIT_HPP
+#define SPLIT_HPP
+
 #include "Sequence.hpp"
 
 template <typename Key, typename Info>
-void split_pos(const Sequence<Key, Info> &seq, int start_pos, int len1, int len2, int count, Sequence<Key, Info> &seq1, Sequence<Key, Info> &seq2)
+void split_pos(/*const*/ Sequence<Key, Info> &seq, int start_pos, int len1, int len2, int count, Sequence<Key, Info> &seq1, Sequence<Key, Info> &seq2)
 {
     typename Sequence<Key, Info>::Iterator it = seq.begin();
     Sequence<Key, Info> new_sequence;
@@ -16,14 +19,14 @@ void split_pos(const Sequence<Key, Info> &seq, int start_pos, int len1, int len2
     }
 
     // Copy elements to seq2
-    int copied_count = 0;
-    bool coping_to_seq1 = true;
+
     for (int c = 0; c < count; c++)
     {
+        int copied_count = 0;
         while (copied_count < len1)
         {
             seq1.push_back(it.key(), it.info());
-            seq.it++;
+            it++;
             copied_count++;
         }
         copied_count = 0;
@@ -35,7 +38,7 @@ void split_pos(const Sequence<Key, Info> &seq, int start_pos, int len1, int len2
         }
     }
 
-    while (it != seq.end())
+    while (it != seq.empty())
     {
         new_sequence.push_back(it.key(), it.info());
         ++it;
@@ -43,3 +46,50 @@ void split_pos(const Sequence<Key, Info> &seq, int start_pos, int len1, int len2
 
     seq = new_sequence;
 }
+
+template <typename Key, typename Info>
+void split_key(/*const*/ Sequence<Key, Info> &seq, const Key &start_key, int start_occ, int len1, int len2, int count, Sequence<Key, Info> &seq1, Sequence<Key, Info> &seq2)
+{
+    typename Sequence<Key, Info>::Iterator target = seq.begin();
+
+    if (!seq.find(target, start_key, start_occ))
+    {
+        throw std::runtime_error("Target not found");
+    }
+
+    Sequence<Key, Info> new_sequence;
+    for (auto it = seq.begin(); it != target; it++)
+    {
+        new_sequence.push_back(it.key(), it.info());
+    }
+    auto it = target;
+    for (int c = 0; c < count; c++)
+    {
+
+        int copied_count = 0;
+        while (copied_count < len1)
+        {
+            seq1.push_back(it.key(), it.info());
+            it++;
+            copied_count++;
+        }
+
+        copied_count = 0;
+        while (copied_count < len2)
+        {
+            seq2.push_back(it.key(), it.info());
+            it++;
+            copied_count++;
+        }
+    }
+
+    while (it != seq.empty())
+    {
+        new_sequence.push_back(it.key(), it.info());
+        ++it;
+    }
+
+    seq = new_sequence;
+}
+
+#endif // SPLIT_HPP
