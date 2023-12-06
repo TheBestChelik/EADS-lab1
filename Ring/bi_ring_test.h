92af840bc3f39cb94772a12218d2bf1b0dca592c
Oleksandr <vasa22292@gmail.com>
2023-12-06 13:58:58 +0100

    feat(BiRing test): add declaration of tests

---
+++ Ring/bi_ring_test.h
@@ -0,0 +1,19 @@
+#pragma once
+void insert_test();
+void erase_test();
+void push_back_test();
+void push_front_test();
+void pop_back_test();
+void pop_front_test();
+void clear_test();
+void iterator_operators_test();
+void iterator_decrement_test();
+void iterator_increment_test();
+void copy_constructor_test();
+void find_key_test();
+void occurrencesOf_test();
+void print_test();
+
+// additional function test
+
+void split_test();
