53eedc69f0bfcbea55e97253e69ee13be5396511
Oleksandr <vasa22292@gmail.com>
2024-01-10 00:03:15 +0100

    feat(ALV): add tests

--- AVL/avl_tree_test.cpp
+++ AVL/avl_tree_test.cpp
@@ -193,6 +193,198 @@ void test_copyconstructor()
     cout << "All tests passed!" << endl;
 }
 
+void test_print()
+{
+    avl_tree<int, std::string> tree;
+    tree.insert(10, "A");
+    tree.insert(5, "B");
+    tree.insert(15, "C");
+    tree.insert(2, "D");
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+
+    // Use ostringstream to capture the output
+    std::ostringstream output;
+    output << tree;
+
+    // Check the printed output
+    std::string expectedOutput =
+        "            18:G\n"
+        "      15:C\n"
+        "            12:F\n"
+        " 10:A\n"
+        "            8:E\n"
+        "      5:B\n"
+        "            2:D\n";
+
+    assert(output.str() == expectedOutput);
+
+    std::cout << "Print test passed!" << std::endl;
+}
+
+void test_get_largest()
+{
+    avl_tree<int, std::string> tree;
+    tree.insert(10, "A");
+    tree.insert(5, "B");
+    tree.insert(15, "C");
+    tree.insert(2, "D");
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+
+    int n = 3;
+    std::vector<pair<int, string>> largestElements = tree.getLargest(n);
+
+    std::vector<pair<int, string>> expectedElements = {pair<int, string>(18, "G"), pair<int, string>(15, "C"), pair<int, string>(12, "F")};
+
+    assert(largestElements == expectedElements);
+
+    largestElements = tree.getLargest(0);
+    expectedElements = {};
+    assert(largestElements == expectedElements);
+
+    largestElements = tree.getLargest(10); // more then in array
+    expectedElements = {pair<int, string>(18, "G"), pair<int, string>(15, "C"), pair<int, string>(12, "F"),
+                        pair<int, string>(10, "A"), pair<int, string>(8, "E"), pair<int, string>(5, "B"), pair<int, string>(2, "D")};
+    assert(largestElements == expectedElements);
+
+    cout << "All get largest tests passed!" << endl;
+}
+
+void test_get_smallest()
+{
+    avl_tree<int, std::string> tree;
+    tree.insert(10, "A");
+    tree.insert(5, "B");
+    tree.insert(15, "C");
+    tree.insert(2, "D");
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+
+    int n = 3;
+    std::vector<std::pair<int, std::string>> smallestElements = tree.getSmallest(n);
+
+    std::vector<std::pair<int, std::string>> expectedElements = {
+        std::make_pair(2, "D"),
+        std::make_pair(5, "B"),
+        std::make_pair(8, "E")};
+
+    assert(smallestElements == expectedElements);
+
+    smallestElements = tree.getSmallest(0);
+    expectedElements = {};
+    assert(smallestElements == expectedElements);
+
+    smallestElements = tree.getSmallest(10); // more than in the array
+    expectedElements = {
+        std::make_pair(2, "D"),
+        std::make_pair(5, "B"),
+        std::make_pair(8, "E"),
+        std::make_pair(10, "A"),
+        std::make_pair(12, "F"),
+        std::make_pair(15, "C"),
+        std::make_pair(18, "G")};
+
+    assert(smallestElements == expectedElements);
+
+    std::cout << "All get smallest tests passed!" << std::endl;
+}
+
+void test_for_each()
+{
+    avl_tree<int, std::string> tree;
+    tree.insert(10, "A");
+    tree.insert(5, "B");
+    tree.insert(15, "C");
+    tree.insert(2, "D");
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+
+    // Use a vector to store the keys and infos during for_each
+    std::vector<std::pair<int, std::string>> elements;
+
+    // Define a lambda function to accumulate keys and infos into the vector
+    auto accumulateFn = [&elements](const int &key, const std::string &info)
+    {
+        elements.push_back(std::make_pair(key, info));
+    };
+
+    // Perform the for_each operation on the tree
+    tree.for_each(accumulateFn);
+
+    // Define the expected elements in the order of in-order traversal
+    std::vector<std::pair<int, std::string>> expectedElements = {
+        {2, "D"},
+        {5, "B"},
+        {8, "E"},
+        {10, "A"},
+        {12, "F"},
+        {15, "C"},
+        {18, "G"}};
+
+    // Check if the elements vector matches the expected elements
+    assert(elements == expectedElements);
+
+    std::cout << "For_each test passed!" << std::endl;
+}
+
+void test_maxinfo_selector()
+{
+    avl_tree<int, std::string> tree;
+    tree.insert(10, "A");
+    tree.insert(5, "B");
+    tree.insert(15, "C");
+    tree.insert(2, "D");
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+
+    int n = 3;
+    std::vector<std::pair<int, std::string>> selectedElements = maxinfo_selector(tree, n);
+
+    // Define the expected elements with the largest info
+    std::vector<std::pair<int, std::string>> expectedElements = {
+        {18, "G"},
+        {12, "F"},
+        {8, "E"}};
+
+    assert(selectedElements == expectedElements);
+
+    // Test when n is greater than the number of elements in the tree
+    selectedElements = maxinfo_selector(tree, 10);
+    expectedElements = {
+        {18, "G"},
+        {12, "F"},
+        {8, "E"},
+        {2, "D"},
+        {15, "C"},
+        {5, "B"},
+        {10, "A"}};
+
+    assert(selectedElements == expectedElements);
+
+    // Test when n is 0
+    selectedElements = maxinfo_selector(tree, 0);
+    expectedElements = {};
+    assert(selectedElements == expectedElements);
+
+    // Test when info can be the same, keys should sum
+    tree[2] = "F";
+    selectedElements = maxinfo_selector(tree, n);
+
+    // Define the expected elements with the largest info
+    expectedElements = {
+        {18, "G"},
+        {14, "F"},
+        {8, "E"}};
+
+    std::cout << "maxinfo_selector test passed!" << std::endl;
+}
+
 int main()
 {
     test_clear_get_size();
@@ -201,40 +393,13 @@ int main()
     test_remove();
     test_assignment_operator();
     test_copyconstructor();
-    cout << "All tests passed!" << endl;
-    // avl_tree<int, int> my_first_tree;
+    test_print();
+    test_get_largest();
+    test_get_smallest();
+    test_for_each();
+    cout
+        << "All tests passed!" << endl;
 
-    // my_first_tree.insert(10, 0);
-    // my_first_tree.insert(9, 0);
-    // my_first_tree.insert(8, 0);
-    // my_first_tree.insert(7, 0);
-    // my_first_tree.insert(6, 0);
-    // my_first_tree.insert(5, 0);
-    // my_first_tree.insert(4, 0);
-    // my_first_tree.insert(3, 0);
-    // my_first_tree.insert(2, 0);
-    // my_first_tree.insert(1, 0);
-
-    // my_first_tree.insert(11, 0);
-    // my_first_tree.insert(12, 0);
-    // assert(my_first_tree.isBalanced());
-
-    // cout << "Start printing" << endl;
-    // cout << my_first_tree << endl;
-    // cout << "Oparetion 1 done" << endl;
-
-    // my_first_tree.remove(10);
-    // assert(my_first_tree.isBalanced());
-    // my_first_tree.remove(9);
-    // assert(my_first_tree.isBalanced());
-    // my_first_tree.remove(8);
-    // assert(my_first_tree.isBalanced());
-    // my_first_tree.remove(7);
-    // assert(my_first_tree.isBalanced());
-    // my_first_tree.remove(3);
-    // assert(my_first_tree.isBalanced());
-
-    // cout << "Start printing" << endl;
-    // cout << my_first_tree << endl;
-    // cout << "Oparetion 2 done" << endl;
+    // External functions tests
+    test_maxinfo_selector();
 }
