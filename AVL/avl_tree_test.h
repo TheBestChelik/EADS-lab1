1a1d8e5095c9dba746962fd1941fea51723307b5
Oleksandr <vasa22292@gmail.com>
2024-01-10 01:52:09 +0100

    Initial commit

---
+++ AVL/avl_tree_test.h
@@ -0,0 +1,13 @@
+#pragma once
+void test_insert();
+void test_remove();
+void test_clear();
+void test_find();
+void test_find_key();
+void test_assignment_operator();
+void test_copy_constructor();
+void test_print();
+void test_get_largest();
+void test_get_smallest();
+void test_for_each();
+void test_word_count();
